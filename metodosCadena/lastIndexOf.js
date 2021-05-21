c26afdcf751a2143cf69710d56ac6a817db9bf06
diego duque <duqueg24@gmail.com>
2021-05-21 17:21:14 -0500

    se añadio el resultado del metodo de cadena

--- metodosCadena/lastIndexOf.js
+++ metodosCadena/lastIndexOf.js
@@ -2,4 +2,4 @@ let cadena = "pedro es un tarado tarado tarado";
 let cadena2 = "tarado";
 
 const resultado = cadena.lastIndexOf(cadena2);
-document.write(resultado);
+document.write(resultado);//26
