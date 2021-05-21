3b4f609a08b64da240e855f82ab83cab8b827c7b
diego duque <duqueg24@gmail.com>
2021-05-21 17:40:21 -0500

    se añadio dos metodos de cadena padStart-padEnd

---
+++ metodosCadena/padEnd.js
@@ -0,0 +1,5 @@
+let cadena = "def";
+let cadena2 = "tarado";
+
+const resultado = cadena.padEnd(6, "abc");
+document.write(resultado); // defabc
---
+++ metodosCadena/padStart.js
@@ -0,0 +1,5 @@
+let cadena = "def";
+let cadena2 = "tarado";
+
+const resultado = cadena.padStart(6, "abc");
+document.write(resultado);// abcdef
