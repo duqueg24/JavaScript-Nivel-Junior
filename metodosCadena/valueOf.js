ed3872a90a339d8701b02d8fecb0e33544a4fbaf
diego duque <duqueg24@gmail.com>
2021-05-21 22:42:43 -0500

    correpciones

--- metodosCadena/valueOf.js
+++ metodosCadena/valueOf.js
@@ -1,6 +1,6 @@
-const stringObj = new String("foo");
+const stringObj = new String('foo');
 
-document.write(stringObj + "<br>");
+document.write(stringObj +"<br>");
 // expected output: String { "foo" }
 
 document.write(stringObj.valueOf());
