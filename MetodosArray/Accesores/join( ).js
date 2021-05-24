2aae9ff178886c236c6e647bd493dfabf4ba71c0
diego duque <duqueg24@gmail.com>
2021-05-23 22:11:42 -0500

    modifico la arquictectura,se agregaron los metodos accesores

---
+++ MetodosArray/Accesores/join( ).js
@@ -0,0 +1,4 @@
+let numeros = [1, 2, 3, 4, 5];
+document.write(numeros + "<br>");//1,2,3,4,5
+let resultado = numeros.join("-");//1-2-3-4-5
+document.write(resultado);// este metodo nos separa por el parametro enviado
---
+++ MetodosArray/Accesores/slice( ).js
@@ -0,0 +1,7 @@
+let numeros = [1, 2, 3, 4, 5];
+document.write(numeros + "<br>"); //1,2,3,4,5
+let resultado = numeros.slice(0,3); //1,2,3
+document.write(resultado);// Vuelve una cadena de texto
+// primer parametro es desde donde comienza
+// sengundo parametro es hasta donde covierte
+// sin incluir esa pasicion
