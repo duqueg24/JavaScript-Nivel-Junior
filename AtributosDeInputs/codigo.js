23811365ec6dde1a4f5e9ce95e0531110577735b
diego duque <duqueg24@gmail.com>
2021-06-10 13:12:23 -0500

    Se modifico el proyecto añadiendo eslint y prettier

---
+++ AtributosDeInputs/codigo.js
@@ -0,0 +1,15 @@
+const input = document.querySelector(".input-normal");
+document.write(input.accept="image/jpg");
+
+//input.className establece o devuelve el nombre de clase de
+//un elemento (el valor del atributo de clase de un elemento).
+
+// input.value La propiedad de valor establece o devuelve
+// el valor del atributo de valor de un campo de texto.
+
+//input.type cambio el tipo de input ya sea text a range
+
+// input.accept="image/jpg" demarca el tipo de archivo de
+// que se va subir.
+
+//jjf
