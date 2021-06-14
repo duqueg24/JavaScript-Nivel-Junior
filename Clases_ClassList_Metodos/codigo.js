67afd3e9ce28993e44b4f45ae8f6de56caba1ec8
diego duque <duqueg24@gmail.com>
2021-06-14 18:16:45 -0500

    Se modifico el proyecto añadiendo Clases_ClassList_Metodos

---
+++ Clases_ClassList_Metodos/codigo.js
@@ -0,0 +1,32 @@
+const tittle = document.querySelector(".titulo");
+/* tittle.classList.add("grande");
+añade una clase
+ .grande {
+    font-size: 50px;
+}
+*/
+
+/* tittle.classList.remove("grande"); remueve una clase si esta estuviera incluida en class="titulo grande"  */
+
+/* let valor = tittle.classList.item(1);
+document.write(valor) 
+nos devueve el nombre de la clase que esta el indice especificado 
+*/
+
+/* let valor = tittle.classList.contains("grande");
+document.write(valor); 
+nos devueve si una clase esta contenida con un valor booleano
+*/
+
+/* tittle.classList.toggle("grande"); 
+en caso de que la clase este contenida la remueve o lo contrio la agrega
+tambien se puede fozar ("grande",true) para que la deje en caso de que este
+("grande",false) para que no la agregue en caso de que no este
+*/
+
+/* let valor = tittle.classList.replace("grande","pr")
+document.write(valor);
+reemplaza una clase por otra
+tittle.classList.replace("grande","pr")
+de vuelve true o false
+*/
---
+++ propiedadStyle/codigo.js
@@ -0,0 +1,5 @@
+const tittle = document.querySelector(".titulo");
+tittle.style.color = "red";
+tittle.style.backgroundColor = "#32b";
+tittle.style.padding = "50px";
+tittle.style.textAlign = "center";
